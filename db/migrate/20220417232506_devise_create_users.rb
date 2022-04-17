7974e1e34fe891671230765b9486307f4b43f7ba
ryanguo1998 <rguo98@uchicago.edu>
2022-04-17 23:27:31 +0000

    before user commit

--- db/migrate/20220417232506_devise_create_users.rb
+++ db/migrate/20220417232506_devise_create_users.rb
@@ -2,6 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
+    enable_extension("citext")
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -33,15 +34,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :username
-      t.boolean :private
-      t.integer :likes_count
-      t.integer :comments_count
+      t.boolean :private, default: true
+      t.integer :likes_count, default: 0
+      t.integer :comments_count, default: 0
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username, unique:true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
