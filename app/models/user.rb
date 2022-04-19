fd7d689d1f23d4647607d96e32933aae6c17fb46
ryanguo1998 <rguo98@uchicago.edu>
2022-04-19 00:37:27 +0000

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,6 @@ class User < ApplicationRecord
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
   has_many :feed, through: :leaders, source: :own_photos
   has_many :discover, through: :leaders, source: :liked_photos
+
+  validates :username, presence: true, uniqueness: true
 end
