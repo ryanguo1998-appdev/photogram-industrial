bad6704fe04d76cec733d12c1263835d3353be40
ryanguo1998 <rguo98@uchicago.edu>
2022-04-19 00:25:22 +0000

    added direct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,24 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  comments_count         :integer          default(0)
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  username               :string
-#  private                :boolean          default(TRUE)
 #  likes_count            :integer          default(0)
-#  comments_count         :integer          default(0)
+#  photos_count           :integer          default(0)
+#  private                :boolean          default(TRUE)
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
@@ -22,5 +29,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id
   has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id
-  
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :fan_id
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
 end
