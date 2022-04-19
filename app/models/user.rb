03f70e2ffd8142ed9f1bbae258b98cadc7902441
ryanguo1998 <rguo98@uchicago.edu>
2022-04-19 00:34:18 +0000

    added indirect associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, class_name: "FollowRequest", foreign_key: :sender_id
   has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, class_name: "FollowRequest", foreign_key: :recipient_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :fan_id
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+  has_many :liked_photos, through: :likes, source: :photo
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+  has_many :feed, through: :leaders, source: :own_photos
+  has_many :discover, through: :leaders, source: :liked_photos
 end
